798e2d8302da83345dcf310161172aab82a7f3a3
Mayu Tatsumi <mtatsumi@hmc.edu>
2025-09-02 12:16:57 -0700

    Fixed blinking frequency to 2.41 Hz

--- lab1/source/impl_1/leds.sv
+++ lab1/source/impl_1/leds.sv
@@ -6,34 +6,25 @@ module leds(
 
 	logic int_osc;
 	logic [31:0] counter;
-	// logic [31:0] q;
 	
-	// 24*10^6 * 430 / 2^32 = 2.402 Hz
-	// 24MHz oscillator
-	// 2 (up + down), 2.4 Hz goal
-	// check 5 MHz = 5,000,000
+	// given 2.4 Hz goal, 24 MHz oscillator
+	// need 2 oscillations
+	// 24 * 10^6 / 2.4 / 2 = 5,000,000
 	
+	// counter = 0 ~ 5,000,000 : OFF
+	// counter = 5,000,000 ~ 10,000,000 : ON
+		
 	HSOSC #(.CLKHF_DIV(2'b01))
 		hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 		
-	// localparam p = 430;
-		
-	// always_ff @(posedge int_osc) begin
-	// 	if (reset) 		q <= 0;
-	//	else if (q < 32'd13760) 	q <= q+p;
-	//	else 	   		q <= 0;
-	// end
-	
-	// posedge int_osc
 	always_ff @(posedge int_osc) begin
-		if      (reset == 0)   counter <= 0;
-		// else if (~counter[22]) counter <= counter + 1;
-		// else 			       counter <= 0;
-		else                   counter <= counter + 1;
+		if      (reset == 0)             counter <= 1'b0;
+		else if (counter < 32'd10000000) counter <= counter + 1;
+		else 			                 counter <= 1'b0;
 	end
-			
-	assign led[2] = counter[22];
 	
+	assign led[2] = (counter > 32'd5000000);
+
 	assign led[0] = ~s[0] ^ ~s[1];
 	assign led[1] = ~s[2] & ~s[3];
 	
